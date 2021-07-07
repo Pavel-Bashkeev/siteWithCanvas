3731579a881fffb00aa689ad2d75d964f566ea59
Pavel-Bashkeev <pavelbashkeev@gmail.com>
2021-07-08 00:14:33 +0300

    Сделан слайдер

--- app/js/vendor/slider.js
+++ app/js/vendor/slider.js
@@ -1,21 +1,13 @@
 const swiper = new Swiper('.swiper-container', {
 	// Optional parameters
-	direction: 'vertical',
-	loop: true,
-
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
-
+	direction: 'horizontal',
+	loop: false,
+	slidesPerView: 2,
+	spaceBetween: 20,
+	width: 860,
 	// Navigation arrows
 	navigation: {
 		nextEl: '.swiper-button-next',
 		prevEl: '.swiper-button-prev',
-	},
-
-	// And if we need scrollbar
-	scrollbar: {
-		el: '.swiper-scrollbar',
 	},
 });
